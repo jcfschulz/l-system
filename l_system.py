45703c9c90bce5d4ecd0eef41e4e4ad9d6cac024
Julius Schulz <julius.schulz@fu-berlin.de>
2015-04-21 18:07:52 +0200

    switched to line drawing, so invisible turtle step is possible

--- l_system.py
+++ l_system.py
@@ -1,6 +1,8 @@
 import numpy as np
-import pylab as plt
 import time
+
+import pylab as plt
+import matplotlib.lines as plt_lines
 
 def string_replacement(axiom, productions, n): 
   s = axiom
@@ -14,7 +16,7 @@ def string_replacement(axiom, productions, n):
     s = snew
   return s
 
-def turtle(s, delta, d, draw_on="F"):
+def turtle_points(s, delta, d, draw_on="F"):
   start = np.array([0.,0.])
   turn = lambda delta: np.round( np.array([[np.cos(delta),-np.sin(delta)],[np.sin(delta),np.cos(delta)]]), 10 )
   v = np.array([0.,d])
@@ -29,6 +31,24 @@ def turtle(s, delta, d, draw_on="F"):
     elif draw_on.find(c)>=0:
       points.append(points[-1]+v)
   return points
+
+def turtle_lines(s, delta, d, draw_on="F", draw_off="f"):
+  start = np.array([0.,0.])
+  turn = lambda delta: np.round( np.array([[np.cos(delta),-np.sin(delta)],[np.sin(delta),np.cos(delta)]]), 10 )
+  v = np.array([0.,d])
+  lines = []
+  p = start
+  for c in s:
+    if c=="-":
+      v = np.dot(v,turn(delta))
+    elif c=="+":
+      v = np.dot(v,turn(-delta))
+    elif draw_off.find(c)>=0:
+      p = p+v
+    elif draw_on.find(c)>=0:
+      lines.append([p,p+v])
+      p = p+v
+  return lines
 
 #axiom = "-L"
 #productions = {"L": "LF+RFR+FL-F-LFLFL-FRFR+", "R": "-LFLF+RFRFR+F+RF-LFL-FR"}
@@ -75,36 +95,40 @@ def turtle(s, delta, d, draw_on="F"):
 #delta = np.pi/2.
 #d = 1.
 
-axiom = "---F"
-productions = {"F": "F++F----F++F"}
-delta = np.pi/6.
-d = 1.
+#axiom = "---F"
+#productions = {"F": "F++F----F++F"}
+#delta = np.pi/6.
+#d = 1.
 
-axiom = "F++F++F"
-productions = {"F": "F-F++F-F"}
-delta = np.pi/3.
-d = 1.
+#axiom = "F++F++F"
+#productions = {"F": "F-F++F-F"}
+#delta = np.pi/3.
+#d = 1.
 
-axiom = "F-F-F-F"
-productions = {"F": "FF-F-F-F-FF"}
-delta = np.pi/2.
-d = 1.
-
-#axiom = "F+F+F+F"
-#productions = {"F": "F+f-FF+F+FF+Ff+FF-f+FF-F-FF-Ff-FFF", "f": "ffffff"}
+#axiom = "F-F-F-F"
+#productions = {"F": "FF-F-F-F-FF"}
 #delta = np.pi/2.
 #d = 1.
 
-n = 5
+axiom = "F+F+F+F"
+productions = {"F": "F+f-FF+F+FF+Ff+FF-f+FF-F-FF-Ff-FFF", "f": "ffffff"}
+delta = np.pi/2.
+d = 1.
+
+n = 2
 t1 = time.time()
 s = string_replacement(axiom, productions, n)
 t2 = time.time()
 print "time for string replacement: ", t2-t1, "s" 
-points = turtle(s, delta, d, "FG")
+lines = turtle_lines(s, delta, d, "FG")
 t3 = time.time()
-print "number of points: ", len(points)
-print "time for point creation: ", t3-t2, "s" 
-  
-plt.plot(*zip(*points))
+print "number of lines: ", len(lines)
+print "time for lines creation: ", t3-t2, "s" 
+
+
+fig, ax = plt.subplots()
+for l in lines:
+  #ax.add_line(plt_lines.Line2D(*zip(*l), color='blue'))
+  plt.plot(*zip(*l), color="b")
 plt.margins(x=.1, y=.1)
 plt.show()
