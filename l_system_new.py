df2d7cce16ee893e23c4ce3ac022994ddc5d3629
Julius Schulz <julius.schulz@fu-berlin.de>
2015-05-08 19:02:55 +0200

    stuff

--- l_system_new.py
+++ l_system_new.py
@@ -1,7 +1,8 @@
 import numpy as np
 import time
-import collada
+#import collada
 import re
+from asteval import Interpreter
 
 #import pylab as plt
 #import matplotlib.lines as plt_lines
@@ -20,25 +21,16 @@ def string_replacement_context(sstart, productions, n, context_ignores=()):
       snew += productions[c]
     else:
       snew += c
-    if !context_ignores.has_key(c):
+    if not context_ignores.has_key(c):
       context = c
     i += 1
   return snew
 
-axiom = "A(1)"
-#productions = {"A": {"params": (a), "expression": "A(a+1)B(2*a)", ""   }   }
-rule = ("S(i,j,k)","S(a,b,c)",None,"i<a, j>c","F[I(i+1)]")
-
-def multi_split(a,delims,split_on=","):
-  for c in delims:
-    a=str.replace(a,c,split_on)
-  return a.split(split_on)
-
-def obtain_symbols(symbols, rule, expr):
+def obtain_symbols(symbols, rule, expr, split_on="\(|,|\)"):
   if rule==None:
     return True
-  rule_split = re.split("\(|,|\)",rule.replace(" ",""))  # do for all whitespaces
-  expr_split = re.split("\(|,|\)",expr.replace(" ",""))  # do for all whitespaces
+  rule_split = re.split(split_on,rule.replace(" ",""))  # do for all whitespaces
+  expr_split = re.split(split_on,expr.replace(" ",""))  # do for all whitespaces
   if expr_split[0]!=rule_split[0]:
     return False
   if len(expr_split)!=len(rule_split):
@@ -50,37 +42,16 @@ def obtain_symbols(symbols, rule, expr):
     symbols[rule_split[i]] = float(expr_split[i])
   return True
 
-def eval_cond_expr(expr, symbols):
-  # parse brackets
-
-  found_prev = False
-  comparing_operators = ("<",">","!=","==")
-  op = ""
-  for c in comparing_operators:
-    cnt = expr.count(c)
-    if cnt>1 or (cnt>0 and found_prev==True):
-      print "Wrong usage of operator", c, ". Maybe use brackets?"   # raise error
-      return 
-    elif cnt==1:
-      expr_split = expr.split(c)
-      op = c
-      found_prev = True
-
-  if op=="<":
-    return symbols[expr_split[0]]<symbols[expr_split[1]]
-  elif op==">":
-    return symbols[expr_split[0]]>symbols[expr_split[1]]
-  elif op=="==":
-    return symbols[expr_split[0]]==symbols[expr_split[1]]
-  elif op=="!=":
-    return symbols[expr_split[0]]!=symbols[expr_split[1]]
-  else:
-    print "unknown operator", op   # raise error
-    return
-
-def eval_math(expr, symbols):
-  operands = expr.split("+")
-  return symbols[operands[0]]+symbols[operands[1]]
+def split_on_brackets(string):
+  res = []
+  s = ""
+  openbrackets = 0
+  closebrackets = 0
+  for c in s:
+    s += c
+    if c="(":
+      res.append(s)
+      s = ""
 
 def match_rule(rule, expression, pre_context, post_context):
   global symbols
@@ -93,20 +64,30 @@ def match_rule(rule, expression, pre_context, post_context):
   if not obtain_symbols(symbols,rule[2],post_context):
     return False
 
-  if conditions != None:
+  aeval.symtable = symbols
+
+  if rule[3] != None:
     conditions = rule[3].replace(" ","").split(",")
     for cond in conditions:
-      if not eval_cond_expr(cond, symbols):
+      #if not eval_cond_expr(cond, symbols):
+      if not aeval(cond):
         return False
 
   sfinal = ""
   res_split = re.split("\(|,|\)",rule[4].replace(" ",""))
   if len(res_split)<2:
     return rule[4]
+  print rule[4]
   for i in range(len(res_split)/2):
     sfinal += res_split[2*i]
     sfinal += "("
-    sfinal += str(eval_math(res_split[2*i+1], symbols))
+    sfinal += str(aeval(res_split[2*i+1]))
     sfinal += ")"
 
   return sfinal
+
+aeval = Interpreter()
+axiom = "A(1)"
+#productions = {"A": {"params": (a), "expression": "A(a+1)B(2*a)", ""   }   }
+rule = ("S(i,j,k)","S(a,b,c)",None,"i<a, j>c","F[I(  2*(i+1)   )]")
+match_rule(rule, "S(5,6,7)", "S(1,10,3)", "A")
